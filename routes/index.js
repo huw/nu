fbce31ae9f7900d412d234c1e01fe6d056e83c8b
Huw Evans <huw1999@gmail.com>
2014-02-13 18:11:21 +1100

    Changed avatar system to Gravatar
    This lets the user download the profile image from a better server
    
    
    Former-commit-id: 7a04adc3c4b15ad1c4a60ae3cb99b438c978dd74

--- routes/index.js
+++ routes/index.js
@@ -17,28 +17,7 @@ exports.index = function(req, res){
         });
     };
 
-	request('https://www.googleapis.com/plus/v1/people/116435831516205433308?key=AIzaSyCkKwR51ZTzPbUzCWkidPl0a50MXhzGwwM', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			bodyArray = JSON.parse(body);
-            imageURL = bodyArray.image.url.replace("50", "300");
-            coverURL = bodyArray.cover.coverPhoto.url;
-
-            fs.readFile('settings.txt', 'utf8', function (err, data) {
-                if (err) throw err;
-                var data = JSON.parse(data);
-                if (imageURL != data.imageURL) {
-                    download(imageURL, 'public/images/profile.png');
-                    data.imageURL = imageURL;
-                    fs.writeFile('settings.txt', JSON.stringify(data), function (err) {
-                        if (err) throw err;
-                    });
-                    res.render('index', {});
-                } else {
-                    res.render('index', {});
-                }
-            });
-		}
-	});
+    res.render('index', {});
 };
 
 /*exports.userlist = function(db) {
