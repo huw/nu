450bcdef7a7c1d9a6fe89fef1de82235beb2b307
Huw <me@huw.nu>
2018-07-03 22:02:40 +1000

    Change index page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,25 +8,53 @@ import { rhythm, scale } from '../utils/typography';
 const Template = (props) => {
   const { children } = props;
 
+  const bioLinkStyle = {
+    marginRight: rhythm(0.5),
+    textDecoration: 'none',
+  };
+
   const header = (
-    <h1
+    <div
       style={{
-        ...scale(1.5),
+        display: 'flex',
+        alignItems: 'end',
+        justifyContent: 'space-between',
         marginBottom: rhythm(1.5),
-        marginTop: 0,
       }}
     >
-      <Link
+      <h1
         style={{
-          boxShadow: 'none',
-          textDecoration: 'none',
-          color: 'inherit',
+          ...scale(2.5),
+          marginTop: 0,
+          marginBottom: 0,
         }}
-        to="/"
       >
-        Huw
-      </Link>
-    </h1>
+        <Link
+          style={{
+            boxShadow: 'none',
+            textDecoration: 'none',
+            color: 'inherit',
+          }}
+          to="/"
+        >
+          huw
+        </Link>
+      </h1>
+      <span>
+        <a href="https://github.com/huw" style={bioLinkStyle}>
+          git
+        </a>
+        <a href="mailto:me@huw.nu" style={bioLinkStyle}>
+          mail
+        </a>
+        <Link to="cv.pdf" style={bioLinkStyle}>
+          cv
+        </Link>
+        <a href="https://keybase.io/huw" style={bioLinkStyle}>
+          gpg
+        </a>
+      </span>
+    </div>
   );
 
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
-import get from 'lodash/get';
 import Helmet from 'react-helmet';
 
-import Bio from '../components/Bio';
+// eslint-disable-next-line import/named
 import { rhythm } from '../utils/typography';
 
-class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-    const posts = get(this, 'props.data.allMarkdownRemark.edges');
+const BlogIndex = (props) => {
+  const {
+    data: {
+      site: {
+        siteMetadata: { title: siteTitle },
+      },
+      allMarkdownRemark: { edges: posts },
+    },
+  } = props;
 
-    return (
-      <div>
-        <Helmet title={siteTitle} />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug;
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>
-                {node.frontmatter.date}
-              </small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Helmet title={siteTitle} />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug;
+        return (
+          <div key={node.fields.slug}>
+            <h3
+              style={{
+                marginBottom: rhythm(1 / 4),
+              }}
+            >
+              <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                {title}
+              </Link>
+            </h3>
+            <small>
+              {node.frontmatter.date}
+            </small>
+            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+BlogIndex.propTypes = {
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.arrayOf([
+        PropTypes.shape({
+          node: PropTypes.shape({
+            frontmatter: PropTypes.shape({
+              title: PropTypes.string.isRequired,
+            }).isRequired,
+            fields: PropTypes.shape({
+              slug: PropTypes.string.isRequired,
+            }).isRequired,
+            excerpt: PropTypes.node.isRequired,
+          }).isRequired,
+        }).isRequired,
+      ]).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default BlogIndex;
 
