df8a513ad8aea69c70717fadde997d49610fb81a
Huw <me@huw.nu>
2018-07-04 23:15:23 +1000

    Add Room 101 + custom excerpts

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,65 +30,80 @@ const PostDate = styled.time`
   margin-bottom: ${rhythm(1 / 4)};
 `;
 
-const BlogIndex = (props) => {
-  const {
-    data: {
-      site: {
-        siteMetadata: { title: siteTitle },
-      },
-      allMarkdownRemark: { edges: posts },
-    },
-  } = props;
-
-  return (
-    <Layout>
-      <div>
-        <Helmet title={siteTitle} />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          return (
-            <div key={node.fields.slug}>
-              <PostTitle>
-                <PostTitleLink to={node.fields.slug}>
-                  {title}
-                </PostTitleLink>
-              </PostTitle>
-              <PostDate dateTime={node.frontmatter.date}>
-                {node.frontmatter.date}
-              </PostDate>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          );
-        })}
-      </div>
-    </Layout>
-  );
-};
-
-BlogIndex.propTypes = {
-  data: PropTypes.shape({
-    site: PropTypes.shape({
-      siteMetadata: PropTypes.shape({
-        title: PropTypes.string.isRequired,
+class BlogIndex extends React.Component {
+  static propTypes = {
+    data: PropTypes.shape({
+      site: PropTypes.shape({
+        siteMetadata: PropTypes.shape({
+          title: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+      allMarkdownRemark: PropTypes.shape({
+        edges: PropTypes.arrayOf([
+          PropTypes.shape({
+            node: PropTypes.shape({
+              frontmatter: PropTypes.shape({
+                title: PropTypes.string.isRequired,
+                date: PropTypes.string.isRequired,
+                excerpt: PropTypes.string,
+              }).isRequired,
+              fields: PropTypes.shape({
+                slug: PropTypes.string.isRequired,
+              }).isRequired,
+              excerpt: PropTypes.node.isRequired,
+            }).isRequired,
+          }).isRequired,
+        ]).isRequired,
       }).isRequired,
     }).isRequired,
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.arrayOf([
-        PropTypes.shape({
-          node: PropTypes.shape({
-            frontmatter: PropTypes.shape({
-              title: PropTypes.string.isRequired,
-            }).isRequired,
-            fields: PropTypes.shape({
-              slug: PropTypes.string.isRequired,
-            }).isRequired,
-            excerpt: PropTypes.node.isRequired,
-          }).isRequired,
-        }).isRequired,
-      ]).isRequired,
-    }).isRequired,
-  }).isRequired,
-};
+  };
+
+  renderExcerpt = (node) => {
+    if (node.frontmatter.excerpt) {
+      return (
+        <p>
+          {node.frontmatter.excerpt}
+        </p>
+      );
+    }
+    return <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />;
+  };
+
+  render() {
+    const {
+      data: {
+        site: {
+          siteMetadata: { title: siteTitle },
+        },
+        allMarkdownRemark: { edges: posts },
+      },
+    } = this.props;
+
+    return (
+      <Layout>
+        <div>
+          <Helmet title={siteTitle} />
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug;
+            return (
+              <div key={node.fields.slug}>
+                <PostTitle>
+                  <PostTitleLink to={node.fields.slug}>
+                    {title}
+                  </PostTitleLink>
+                </PostTitle>
+                <PostDate dateTime={node.frontmatter.date}>
+                  {node.frontmatter.date}
+                </PostDate>
+                {this.renderExcerpt(node)}
+              </div>
+            );
+          })}
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default BlogIndex;
 
@@ -109,6 +124,7 @@ export const pageQuery = graphql`
           frontmatter {
             date(formatString: "D MMMM, YYYY")
             title
+            excerpt
           }
         }
       }
---
+++ src/pages/room-101/index.md
@@ -0,0 +1,13 @@
+---
+title: Room 101
+date: "2016-08-19T00:00:00.000Z"
+excerpt: I wrote a full sitcom screenplay in my last year of high school. It was one of the most rewarding experiences of my life.
+---
+
+I wrote a full sitcom screenplay in my last year of high school. 
+
+Like most other countries, Australian high school students sit a set of exams called the Higher School Certificate — except some of them, like English Extension 2, aren't exams at all.
+
+Instead, we spent the year researching and handcrafting a body of work, learning and understanding the writing, drafting, and re-drafting process as we went. While the deadlines were tight and the criticism was harsh, it was one of the most rewarding experiences I had at high school. I wish all students had an opportunity like this.
+
+It's called [__Room 101__](./room-101.pdf), and it's about a high school English department. Any and all resemblance to real English teachers I know is purely coincidental.
